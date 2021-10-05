1bf126f0dd7821e68a194606a7fb59e822496158
edupaiva <pro.eduardo@hotmail.com>
2021-10-05 16:32:52 -0300

    Plus

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_TITLE: str = "Jobboard | by Eduardo Paiva"
     PROJECT_VERSION: str = "1.1.0"
+
+    POSTRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTBRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTBRES_SERVER: str= os.getenv("POSTGRES_SERVER", "localhost")
+    POSTBRES_PORT: str=os.getenv("POSTGRES_PORT", 5432)
+    POSTBRES_DB: str=os.getenv("POSTGRES_DB","funceme")
+    DATABASE_URL = f"postgresql://{POSTRES_USER}:{POSTBRES_PASSWORD}@{POSTBRES_SERVER}:{POSTBRES_PORT}/{POSTBRES_DB}"
 
 settings = Settings()
