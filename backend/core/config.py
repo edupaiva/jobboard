a28e6183ae5cad1f4f9be2ad82a7d6175d4679db
edupaiva <pro.eduardo@hotmail.com>
2021-10-06 10:29:36 -0300

    Connecting to Postgres and Sqlite databases

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,11 +9,12 @@ class Settings:
     PROJECT_TITLE: str = "Jobboard | by Eduardo Paiva"
     PROJECT_VERSION: str = "1.1.0"
 
-    POSTRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTBRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTBRES_SERVER: str= os.getenv("POSTGRES_SERVER", "localhost")
-    POSTBRES_PORT: str=os.getenv("POSTGRES_PORT", 5432)
-    POSTBRES_DB: str=os.getenv("POSTGRES_DB","funceme")
-    DATABASE_URL = f"postgresql://{POSTRES_USER}:{POSTBRES_PASSWORD}@{POSTBRES_SERVER}:{POSTBRES_PORT}/{POSTBRES_DB}"
+    POSTBRES_USER: str = os.getenv("POSTBRES_USER")
+    POSTBRES_PASSWORD = os.getenv("POSTBRES_PASSWORD")
+    POSTBRES_SERVER: str= os.getenv("POSTBRES_SERVER", "localhost")
+    POSTBRES_PORT: str=os.getenv("POSTBRES_PORT", 5432)
+    POSTBRES_DB: str=os.getenv("POSTBRES_DB","funceme")
+
+    DATABASE_URL = f"postgresql://{POSTBRES_USER}:{POSTBRES_PASSWORD}@{POSTBRES_SERVER}:{POSTBRES_PORT}/{POSTBRES_DB}"
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
+from db.session import engine   #new
+from db.base_class import Base  #new
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
-@app.get("/")
-def hello_api():
-    return{"detail": "Hello World"}
+
+def create_tables():           #new
+	Base.metadata.create_all(bind=engine)
+
+	
+def start_application():
+	app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+	create_tables()       
+	return app
+
+app = start_application()
